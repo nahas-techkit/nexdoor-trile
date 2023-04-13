1c86deb9a80391d41f3a2ba071ea53f150cfe3da
nahas-techkit <nahas@techkit.in>
2023-04-13 15:18:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         This is a trial run for nexdoor
         </a>
       </header>
     </div>
